9c7cebf5bcbc3fa98ccf4c9033ad5541268a2bb9
Unknown <1421365535@qq.com>
2019-01-16 10:29:47 +0800

    调整电角度零点位置，电机加盖哈哈哈

--- F103_Demo/User/main.c
+++ F103_Demo/User/main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-//测得磁编码器对应的机械角0度，对应电角度=>252度（正负1度），机械角度是电角度的14倍。
-//则机械角度与电角度的换算关系为：电角度=[（机械角度/（16384/14））*360+252]%360
+//测得磁编码器对应的机械角0度，对应电角度=>42度（正负1度），机械角度是电角度的14倍。
+//则机械角度与电角度的换算关系为：电角度=[（机械角度/（16384/14））*360+60]%360
 u16 DRV=0,Check=0xBFFF;
 int FOC_T=500;
 long Change=0;
@@ -21,11 +21,17 @@ int main(void)
 //  DelayUs(1);
      
 //  DelayUs(100);
-    Feedback_Theta=((int)(as5048_A.reg/(16384.0/14.0)*360.0+252))%360;  
-      
+    Feedback_Theta=((int)(as5048_A.reg/(16384.0/14.0)*360.0+42))%360;  //机械角度换电角度
+    
+          
     DRV8305.Park.Theta=Feedback_Theta*10+SetAdvance_Theta;
+      
+//    DRV8305.Park.Theta=DRV8305.Park.Theta+3;  
     if(DRV8305.Park.Theta>3599)
-    DRV8305.Park.Theta=0;    
+    DRV8305.Park.Theta=0; 
+
+//    if(as5048_A.reg==16383)
+//    Change= DRV8305.Park.Theta;     
 
     Anti_Park_Calc();     
     Svpwm_Module();      
